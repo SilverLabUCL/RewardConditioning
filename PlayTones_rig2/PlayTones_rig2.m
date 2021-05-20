b7b2558f21686f316d5f73ddfd7835f8931c0c91
harshagurnani <harsha.gurnani.16@ucl.ac.uk>
2021-05-20 22:43:54 +0100

    update tones data

--- PlayTones_rig2/PlayTones_rig2.m
+++ PlayTones_rig2/PlayTones_rig2.m
@@ -50,9 +50,8 @@ function PlayTones_rig2( )
     %% Initiate sounds
    
     PsychToolboxSoundServer('init')
-    [Freq, ID] = generate_cues( S );
+    [Freq,Sounds, ID] = generate_cues( S );
     for jj=ID
-        Sounds{jj} = GenerateSineWave(SF, Freq(jj), S.GUI.SamplePeriod)*.9; % Sampling freq (hz), Sine frequency (hz), duration (s)
         PsychToolboxSoundServer('Load', jj, Sounds{jj});    
     end
     BpodSystem.SoftCodeHandlerFunction = 'SoftCodeHandler_PlaySound';
@@ -64,10 +63,8 @@ function PlayTones_rig2( )
     BpodSystem.Data.TrialOutcomes = []; % The trial outcomes
 
     %% Initialise plots
-    initialise_plots( true, true );
-    PerfOutcomePlot(BpodSystem.GUIHandles.PerfOutcomePlot,1,'init',0);
-    TrialBehPlot( BpodSystem.GUIHandles.LickFig, 1, [] );
-    
+    initialise_plots( true );
+
     % Pause the protocol before starting
     BpodSystem.Status.Pause = 1;
     HandlePauseCondition;
@@ -83,13 +80,11 @@ function PlayTones_rig2( )
         
         S = BpodParameterGUI('sync', S); % Sync parameters with BpodParameterGUI plugin
         disp(['Starting trial ',num2str(currentTrial)])
-        TrigWait = S.GUI.TriggerWait;
-        
+
         
         %load sounds
-        [Freq, ID] = generate_cues( S );
+        [Freq,Sounds, ID] = generate_cues( S );
         for jj=ID
-            Sounds{jj} = GenerateSineWave(SF, Freq(jj), S.GUI.SamplePeriod)*.9; % Sampling freq (hz), Sine frequency (hz), duration (s)
             PsychToolboxSoundServer('Load', jj, Sounds{jj});    
         end
         BpodSystem.Data.allSoundFreq = Freq;
@@ -104,9 +99,6 @@ function PlayTones_rig2( )
         BpodSystem.Data.StimuliFreqs = Freq( currStimuli );
         BpodSystem.Data.StimuliID = currStimuli;
         nStim = length(currStimuli);
-        
-
-        PerfOutcomePlot(BpodSystem.GUIHandles.PerfOutcomePlot,currentTrial,'next_trial',TrialTypes(currentTrial), BpodSystem.GUIHandles.DisplayNTrials);
 
         sma = NewStateMatrix(); % Assemble state matrix
         BpodSystem.Data.TrialSettings(currentTrial) = S; % Adds the settings used for the current trial to the Data struct (to be saved after the trial ends)
@@ -118,7 +110,7 @@ function PlayTones_rig2( )
            
                 
                 sma = AddState(sma, 'Name', 'TrigTrialStart', ...                    
-                    'Timer', interval, ...
+                    'Timer', S.GUI.TriggerWait, ...
                     'StateChangeConditions', {io.TrialTrigger, 'Pre_1', 'Tup', 'TrialEnd'}, ...
                     'OutputActions', {});
 
@@ -179,16 +171,15 @@ function PlayTones_rig2( )
             BpodSystem.Data = AddTrialEvents(BpodSystem.Data,RawEvents); % Computes trial events from raw data
             BpodSystem.Data.TrialSettings(currentTrial) = S; % Add the settings used for the current trial to the Data struct (to be saved after the trial ends)
             
+            if S.GUI.ProtocolType == 1
+                TrialBehPlot(BpodSystem.GUIHandles.ToneFig, BpodSystem.Data.nTrials, currentTrial )
+            end
+            
             SaveBpodSessionData; % Saves the field BpodSystem.Data to the current data file
             BpodSystem.ProtocolSettings = S;
             SaveProtocolSettings(S); % SaveBpodProtocolSettings;
 
         end
-
-        % Pause the protocol before starting if in Water-Valve-Calibration
-        if S.GUI.ProtocolType == 1
-            BpodSystem.Status.Pause = 1;
-        end        
 
         HandlePauseCondition; % Checks to see if the protocol is paused. If so, waits until user resumes.
         if BpodSystem.Status.BeingUsed == 0
@@ -202,22 +193,28 @@ end
 
 %% To add plotting
 
-function TrialBehPlot(ax, Ntrials, trials, lickevent)
+function TrialBehPlot(ax, Ntrials, trials )
 
     global BpodSystem
-    sz = 10;
-    if nargin<4, lickevent = 'BNC2High'; end
-        
-    trigIn = 'BNC1High';
+    
+    fmax = 20; %kHz
+    cm = colormap( jet(fmax) );
+    % f1 = 1000; f2 = 25000;
+    
+    
+%     
+%     ctr=1;
+%     for jj=1:5:fmax
+%        leg(ctr) = plot(ax, NaN, NaN, 'color', cm(jj,:), 'linewidth',1);
+%        lname{ctr} = ['Freq = ', num2str(jj), ' kHz' ];
+%        ctr=ctr+1;
+%     end
     
     Ndisplay = 40;
     ind1 = max(1, Ntrials-Ndisplay+1);
     
     hold(ax, 'on');
     
-    
-    leg(1) = scatter(ax, NaN, NaN, sz, 'r', 'filled');
-
     try
         TrialStart = BpodSystem.Data.RawEvents.Trial{trials}.States.TrigTrialStart(2);
     catch
@@ -225,124 +222,45 @@ function TrialBehPlot(ax, Ntrials, trials, lickevent)
     end
 
     for trial=trials
-    plot(ax, BpodSystem.Data.RawEvents.Trial{1,trial}.States.SamplePeriod - TrialStart, [trial,trial], 'k');
-    end
-    leg(2) = plot(ax, NaN, NaN, 'k');
-
-    for trial=trials
-    if isfield(BpodSystem.Data.RawEvents.Trial{1,trial}.States,'Reward')
-        lw=1;
-        if isfield( BpodSystem.Data.RawEvents.Trial{trial}.Events, lickevent)
-            if any(BpodSystem.Data.RawEvents.Trial{trial}.Events.(lickevent)>=BpodSystem.Data.RawEvents.Trial{trial}.States.Reward(1) ... 
-           & BpodSystem.Data.RawEvents.Trial{trial}.Events.(lickevent)<=BpodSystem.Data.RawEvents.Trial{trial}.States.RewardConsumption(2))
-            lw=3;
-            end
+        nsamples = length(BpodSystem.Data.StimuliID);
+        
+        for jj=1:nsamples
+           freq = BpodSystem.Data.StimuliFreqs(jj);
+            clr = ceil(freq/1000);
+            if clr<1, clr=1; end
+            if clr>fmax, clr = fmax; end
+           
+           state = ['Sample_', num2str(jj)];
+           plot(ax, BpodSystem.Data.RawEvents.Trial{1,trial}.States.(state) - TrialStart, [trial,trial],'color', cm(clr,:), 'linewidth', 1 );
+           
+           
         end
-    plot(ax, BpodSystem.Data.RawEvents.Trial{1,trial}.States.Reward - TrialStart, [trial,trial], 'b', 'linewidth',lw);
-    plot(ax, BpodSystem.Data.RawEvents.Trial{1,trial}.States.RewardConsumption - TrialStart, [trial,trial], 'b', 'linewidth',lw);
-    end
+        
     end
     
-    leg(3) = plot( ax, NaN, NaN, 'b');
-
-    for trial=trials
-    if isfield(BpodSystem.Data.RawEvents.Trial{1,trial}.States,'AnswerPeriod')
-    plot(ax, BpodSystem.Data.RawEvents.Trial{1,trial}.States.AnswerPeriod - TrialStart, [trial,trial], 'g');
-    end
-    end
-    leg(4) = plot(ax,  NaN, NaN, 'g');
-
-    % licks
-    for trial=trials
-    if isfield( BpodSystem.Data.RawEvents.Trial{1,trial}.Events, lickevent), licks = BpodSystem.Data.RawEvents.Trial{1,trial}.Events.(lickevent) - TrialStart; 
-    scatter(ax, licks, ones(size(licks))*trial, sz,'r','filled');
-    end
-    end
     
-    for trial=trials
-    if isfield( BpodSystem.Data.RawEvents.Trial{1,trial}.Events, trigIn) 
-        trig = BpodSystem.Data.RawEvents.Trial{1,trial}.Events.(trigIn) - TrialStart; 
-        scatter(ax, trig, ones(size(trig))*trial, sz,'g','filled');
-    end
-    end
-    
-    leg(5) = scatter(ax,  NaN, NaN, 'g', 'filled');
-
-    
-    legend(ax,  leg, 'Licks', 'SamplePeriod', 'Reward and consumption', 'Answer Period', 'Trial Trigger');
+%     legend( lname );
     ylim(ax, [ind1 ind1+40]);
-    xlim(ax, [-1, 8] );
+        
 end
 
 
-function initialise_plots( doOutcome, doLicks )
+function initialise_plots( doTones )
 
     global BpodSystem
-    if nargin<2, doLicks=true; end
-    if nargin<1, doOutcome=true'; end
+    if nargin<1, doTones=true; end
     
-    if doOutcome
-        BpodSystem.ProtocolFigures.PerfOutcomePlotFig = figure('Position', [150 800 1600 200], ...
-        'name','Outcome plot','numbertitle','off', 'MenuBar', 'none', 'Resize', 'off', 'Color', [1 1 1]);
+    if doTones
+        BpodSystem.ProtocolFigures.ToneFig = figure('Position', [150 400 800 400], ...
+            'name','Trial Frequencies plot','numbertitle','off', 'MenuBar', 'none', 'Resize', 'off', 'Color', [1 1 1]);
 
-        BpodSystem.GUIHandles.PerfOutcomePlot = axes('Position', [.15 .2 .8 .7], 'FontSize', 11);
-
-        uicontrol('Style', 'text', 'String', 'nDisplay: ','Position',[20 170 100 18], ...
-            'HorizontalAlignment', 'left', 'BackgroundColor', [1 1 1], 'FontSize', 11);
-        BpodSystem.GUIHandles.DisplayNTrials = uicontrol('Style','edit','string','100','Position',[125 170 40 18], ...
-            'HorizontalAlignment', 'left', 'BackgroundColor', [1 1 1], 'FontSize', 11);
-
-        uicontrol('Style', 'text', 'String', 'hit % (all): ','Position',[20 140 100 18], ...
-            'HorizontalAlignment', 'left', 'BackgroundColor', [1 1 1], 'FontSize', 11);
-        BpodSystem.GUIHandles.hitpct = uicontrol('Style','text','string','0','Position',[125 140 40 18], ...
-            'HorizontalAlignment', 'left', 'BackgroundColor', [1 1 1], 'FontSize', 11);
-
-        uicontrol('Style', 'text', 'String', 'CR % (all): ','Position',[20 120 100 18], ...
-            'HorizontalAlignment', 'left', 'BackgroundColor', [1 1 1], 'FontSize', 11);
-        BpodSystem.GUIHandles.corrRej = uicontrol('Style','text','string','0','Position',[125 120 40 18], ...
-            'HorizontalAlignment', 'left', 'BackgroundColor', [1 1 1], 'FontSize', 11);
-        
-        
-        uicontrol('Style', 'text', 'String', 'hit % (40): ','Position',[20 90 100 18], ...
-            'HorizontalAlignment', 'left', 'BackgroundColor', [1 1 1], 'FontSize', 11);
-        BpodSystem.GUIHandles.hitpctrecent = uicontrol('Style','text','string','0','Position',[125 90 40 18], ...
-            'HorizontalAlignment', 'left', 'BackgroundColor', [1 1 1], 'FontSize', 11);
-        
-        
-        uicontrol('Style', 'text', 'String', 'CR % (40): ','Position',[20 70 100 18], ...
-            'HorizontalAlignment', 'left', 'BackgroundColor', [1 1 1], 'FontSize', 11);
-        BpodSystem.GUIHandles.corrRejrecent = uicontrol('Style','text','string','0','Position',[125 70 40 18], ...
-            'HorizontalAlignment', 'left', 'BackgroundColor', [1 1 1], 'FontSize', 11);
-        
-        
-
-        uicontrol('Style', 'text', 'String', 'Trials: ','Position',[20 40 100 18], ...
-            'HorizontalAlignment', 'left', 'BackgroundColor', [1 1 1], 'FontSize', 11);
-        BpodSystem.GUIHandles.numtrials = uicontrol('Style','text','string','0','Position',[125 40 40 18], ...
-            'HorizontalAlignment', 'left', 'BackgroundColor', [1 1 1], 'FontSize', 11);
-
-        uicontrol('Style', 'text', 'String', 'Rewards: ','Position',[20 20 100 18], ...
-            'HorizontalAlignment', 'left', 'BackgroundColor', [1 1 1], 'FontSize', 11);
-        BpodSystem.GUIHandles.numRewards = uicontrol('Style','text','string','0','Position',[125 20 40 18], ...
-            'HorizontalAlignment', 'left', 'BackgroundColor', [1 1 1], 'FontSize', 11);
-
-    
+        BpodSystem.GUIHandles.ToneFig = axes('Position', [.15 .2 .8 .7], 'FontSize', 11);
     end
-
-    
-    if doLicks
         
-        BpodSystem.ProtocolFigures.LickFig = figure('Position', [150 400 800 400], ...
-            'name','Trial behaviour plot','numbertitle','off', 'MenuBar', 'none', 'Resize', 'off', 'Color', [1 1 1]);
-
-        BpodSystem.GUIHandles.LickFig = axes('Position', [.15 .2 .8 .7], 'FontSize', 11);
-    end
-    
-    
 end
 
 
-function [Freq, ID] = generate_cues( S )
+function [Freq,Sounds, ID] = generate_cues( S )
     SF = 192000; % Analog module sampling rate
     
     if S.GUI.Freq_Interval<=0, S.GUI.Freq_Interval=500; end
@@ -350,5 +268,9 @@ function [Freq, ID] = generate_cues( S )
     Freq = S.GUI.Freq_Min: S.GUI.Freq_Interval:S.GUI.Freq_Max;
     nFreq = length(Freq);
     ID =1:nFreq;
+    for jj=ID
+        Sounds{jj} = GenerateSineWave(SF, Freq(jj), S.GUI.SamplePeriod)*.9; % Sampling freq (hz), Sine frequency (hz), duration (s)
+        
+    end
     
 end        
